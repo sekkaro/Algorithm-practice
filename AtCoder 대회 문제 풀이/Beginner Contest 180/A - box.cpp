906516015de76cfcc5fca796dfd7510b9f204d73
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-01-15 17:07:58 +0900

    144th commit

---
+++ AtCoder 대회 문제 풀이/Beginner Contest 180/A - box.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define int long long
+
+signed main(void){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n, a, b;
+	cin >> n >> a >> b;
+	cout << n - a + b << '\n';
+	return 0;
+}
