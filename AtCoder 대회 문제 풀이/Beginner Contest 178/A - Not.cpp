6e49a13a3e204274294c545d210869cea561bb37
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-01-24 17:51:46 +0900

    160th commit

---
+++ AtCoder 대회 문제 풀이/Beginner Contest 178/A - Not.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define int long long
+ 
+signed main(void){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int x;
+	cin >> x;
+	if(x == 0) cout << 1 << '\n';
+	else cout << 0 << '\n';
+	return 0;
+}
